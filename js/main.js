8677df00da960ad7cc5a25728b3b66c00494d834
Maksim Karpov <maksimus@rtural.ru>
2020-04-22 12:32:23 +0500

    add style

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
+// Добавьте значения по умолчанию для аргументов функции. Как можно упростить или сократить запись функций? 
+const renderProduct = (title='New_prod', price=9999) => {
   return `<div class="product-item">
+            <a><img src="https://placeimg.com/240/120/any" alt="${title}"></a>
             <h3>${title}</h3>
             <p>${price}</p>
             <button class="by-btn">Добавить в корзину</button>
@@ -14,15 +16,20 @@ const renderProduct = (title, price) => {
 };
 
 const renderProducts = (list) => {
-  // const productList = list.map((good) => {
-  //   return renderProduct(good.title, good.price);
-  // });
-  const productList = [];
-  list.forEach(good => {
-    productList.push(renderProduct(good.title, good.price));
+  const productList = list.map((good) => {
+    return renderProduct(good.title, good.price);
   });
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+  // const productList = [];
+  // list.forEach(good => {
+  //   productList.push(renderProduct(good.title, good.price));
+  // });
+  //  3. Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит? - Это происходит из-за того что мы выводим весь массив элементов и массив включает разделители запятые после каждого элемента 
+  // Как это исправить ? - Это можно исправить сборкой строки из элементов массива. 
+
+  for (let i = 0; i < productList.length; i++) {
+    document.querySelector('.products').innerHTML += productList[i];
+  }
+  
 };
 
 renderProducts(products);
