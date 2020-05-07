317920b73a0704306a09f235ad0fc52bb857a7f8
Maksim Karpov <maksimus@rtural.ru>
2020-05-07 16:37:59 +0500

    Finish Lesson 5

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ const app = new Vue({
     el: '#app',
     data: {
         catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
         products: [],
-        imgCatalog: 'https://placehold.it/200x150'
+        cartProducts: [],
+        imgCatalog: 'https://placehold.it/200x150',
+        imgCartProduct: 'https://placehold.it/50x100',
+        searchLine: '',
+        filtered: [],
+        isVisibleCart: true,
+        noProductCart: true,
+        noCatalogData: true
     },
     methods: {
         getJson(url){
@@ -17,14 +25,42 @@ const app = new Vue({
         },
         addProduct(product){
             console.log(product.id_product);
+        },
+        filterGoods(value) {
+            const regexp = new RegExp(value, 'i');
+            return this.filtered = this.products.filter(product => regexp.test(product.product_name));
+        },
+        isVisible(productId) {
+            if (this.filtered.length != 0) {
+                return !this.filtered.find(product => product.id_product === productId);
+            }
+            return false;
         }
+        
+        
     },
+    
     mounted(){
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
-                for(let el of data){
-                    this.products.push(el);
+                if (data.length == 0) {
+                    return this.noCatalogData = false
+                }
+                for (let el of data) {
+                    this.products.push(el)
                 }
             });
+            
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                if (data.contents.length == 0) {
+                    return this.noProductCart = false
+                }
+                for (let el of data.contents) {
+                    this.cartProducts.push(el);
+                }
+            });
+            
     }
+    
 });
